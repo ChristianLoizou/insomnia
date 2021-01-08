fc8110882c4a881b0ebf18c7e1206fddb8af434e
Christian Alexander Loizou <cloiozu777@gmail.com>
2021-01-08 23:19:55 +0000

    feat|refactor: added link route to new P5.JS sketch, renamed some variables

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import os
 from datetime import datetime
 from functools import lru_cache
 
+import pytz
+from astral import LocationInfo
+from astral.sun import sun
 from flask import Flask, render_template
 
 
 class Template:
-    def __init__(self, name, style=None):
+    def __init__(self, name, style=None, extension="jinja"):
         self.name = name
-        self.path = f"html/{self.name}.jinja"
+        self.path = f"html/{self.name}.{extension}"
         self.style = self.get_stylsheet(style)
 
     def get_stylsheet(self, style):
@@ -27,12 +30,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    current_time = datetime.now().strftime("%H:%M\t%A, %b %-d")
-    # do other stuff here...
-    return Template('index').render(date_time=current_time)
+    current_time, logo_scheme = get_logo_colour_scheme()
+    formatted_time = current_time.strftime("%H:%M\t%A, %b %-d")
+    return Template('index').render(
+        date_time=formatted_time,
+        logo_bubble_back=logo_scheme["bubble_back"],
+        mountain_back=logo_scheme["mountain_back"],
+        projects=projects
+    )
 
 
-@lru_cache()  # this line caches .css files, so if one file is used twice the script doesn't have to load it twice
+@app.route('/p5js/sketch')
+def chess():
+    return Template('p5js_sketch').render()
+
+
+# lru_cache will cache .css files, so if one file is used twice the script doesn't have to load it twice
+@lru_cache(maxsize=None)
 def load_css(stylesheet_name):
     return css[stylesheet_name]
 
@@ -45,6 +59,32 @@ def load_stylesheets(path="styles/"):
     return css
 
 
+def get_logo_colour_scheme(override=None):
+    schemes = {
+        "day": {
+            "bubble_back": "#C1F1FF",
+            "mountain_back": "#2E5F55"
+        },
+        "night": {
+            "bubble_back": "#333388",
+            "mountain_back": "#eeeeee"
+        }
+    }
+    utc = pytz.UTC
+    current_time = datetime.now(utc)
+    if override is not None:
+        return current_time, schemes[override]
+    london = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
+    s = sun(london.observer, date=current_time)
+    if s["dawn"] < current_time <= s["dusk"]:
+        return current_time, schemes["day"]
+    else:
+        return current_time, schemes["night"]
+
+
 if __name__ == "__main__":
     css = load_stylesheets()
+    projects = {
+        "P5.js Sketch": '/p5js/sketch'
+    }
     app.run(debug=True)
