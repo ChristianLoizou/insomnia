c7695a7c69ff782ba97062cd35336bc5ca597a5e
Christian Alexander Loizou <cloiozu777@gmail.com>
2021-01-15 00:53:30 +0000

    feat: turned P5.JS sketch into simple drawing app to test funtionality

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,10 +1,31 @@
-// What to do here...
+const ELLIPSE_SIZE = 25;
+
+var positions;
+
+
 function setup() {
     createCanvas(800, 800);
+    positions = [];
 }
 
 function draw() {
     background(51);
-    rectMode(CENTER);
-    ellipse(mouseX, mouseY, 75, 75, 15);
+    noStroke();
+    fill(120);
+    positions.forEach(function (vector, index) {
+        ellipse(vector.x, vector.y, ELLIPSE_SIZE);
+    });
+    fill(118, 118, 125);
+    ellipse(mouseX, mouseY, ELLIPSE_SIZE);
+
+}
+
+function mousePressed() {
+    position = createVector(mouseX, mouseY);
+    positions.push(position);
+}
+
+function mouseDragged() {
+    position = createVector(mouseX, mouseY);
+    positions.push(position);
 }
